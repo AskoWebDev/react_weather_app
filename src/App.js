0e0da48ad538edceee7046cf2e95a90bc184841c
AskoWebDev <bestgit3@gmail.com>
2022-07-15 19:30:43 +0300

    deploy app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import React, { useState } from 'react';
+
+const api = {
+    key: '1851adabbd4079ebf88df8f7386d0849'
+  }
 
 function App() {
+  const [weather, setWeather] = useState([])
+  const [city, setCity] = useState('')
+  const [time, setTime] = useState()
+  
+
+
+  function fetchapi(evt) {
+    if (city.length === 0) {
+      return
+    }else if(evt.key === 'Enter' || 'button')
+      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${api.key}`)
+        .then(response => response.json())
+        .then(res => {
+          setWeather(res);
+          setTime(new Date())
+          console.log(weather)
+        })
+         
+  }
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ backgroundImage: `url('/public/cold')` }}>
+        <div className="input">
+          <input 
+            onChange={e => setCity(e.target.value)} 
+            value={city}
+            onKeyPress={fetchapi}
+            placeholder="Search"
+          />     
+          <button onClick={fetchapi} name='button'>Search</button>  
+        </div>  
+          {(typeof weather.main != 'undefined') ? (
+          <div className="output">
+              <h1>City: {weather.name}</h1>
+              <h2>{time.toDateString()}</h2>
+              <h2>Temperature: {Math.round(weather.main.temp)}°C</h2> 
+              <h2>{weather.weather[0].main}</h2>
+          </div>
+          
+        ): ('')}
+        
     </div>
   );
 }
